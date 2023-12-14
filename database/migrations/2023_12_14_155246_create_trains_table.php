c6dba5048b37ee893a5ab9d4ccf5c7cd69938fb7
Limson91 <matteo.a.colautti@gmail.com>
2023-12-14 17:21:18 +0100

    add - migrate table trains

--- database/migrations/2023_12_14_155246_create_trains_table.php
+++ database/migrations/2023_12_14_155246_create_trains_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('company');
+            $table->string('depearture_station');
+            $table->string('arrival_station');
+            $table->dateTime('departure_time');
+            $table->dateTime('arrive_time');
+            $table->string('train_code');
+            $table->tinyInteger('coach_number')->unsigned(); //unsigned per impedire numeri negativi
+            $table->boolean('confirmed')->default(true);
+            $table->boolean('cancelled')->default(false);
             $table->timestamps();
         });
     }
